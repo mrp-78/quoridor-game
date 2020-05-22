6200889b98464b0f03c8b81400b9877bdd64d2be
MRSadeghi78 <mary.nzn@gmail.com>
2020-05-22 18:27:44 +0430

    4p addWall function added

--- Quoridor4P.py
+++ Quoridor4P.py
@@ -88,3 +88,38 @@ class Main:
             pm.remove(player4.pos);
 
         return pm;
+
+    def isSurrounded(self, player1, player2, player3, player4, rdes, cdes):
+        blocks = [];
+        visited = [[False for x in range(9)] for y in range(9)];
+        p = Player(player1.pos);    
+        blocks.append(p.pos);
+        while blocks:
+            p.pos = blocks.pop();
+            temp = self.possibleMoves(p, player2, player3, player4);
+            for t in temp:
+                if not visited[t.row][t.col]:
+                    if t.row in rdes or t.col in cdes:
+                        return False;
+                    visited[t.row][t.col] = True;
+                    blocks.append(t); 
+        return True;    
+
+    @staticmethod
+    def addHwall(c1, c2, r1, r2, player1, player2, player3, player4):
+        if Main.hwalls[r1][c1 - 1] or Main.hwalls[r1][c1] or Main.hwalls[r1][c2] or Main.vwalls[r1][c1]:
+            return False;
+        Main.hwalls[r1][c1] = True;
+        if Main.isSurrounded(player1, player2, player3, player4, [8], [0, 8]) or Main.isSurrounded(player2, player1, player3, player4, [0, 8], [0]) or Main.isSurrounded(player3, player2, player1, player4, [0], [0, 8]) or Main.isSurrounded(player4, player2, player3, player1, [0, 8], [8]):
+            Main.hwalls[r1][c1] = False;
+            return False;
+        return True;
+
+    def addVwall(self, c1, c2, r1, r2, player1, player2, player3, player4):
+        if self.vwalls[r1 - 1][c1] or self.vwalls[r1][c1] or self.vwalls[r2][c1] or self.hwalls[r1][c1]:
+            return False;
+        self.vwalls[r1][c1] = True;
+        if Main.isSurrounded(player1, player2, player3, player4, [8], [0, 8]) or Main.isSurrounded(player2, player1, player3, player4, [0, 8], [0]) or Main.isSurrounded(player3, player2, player1, player4, [0], [0, 8]) or Main.isSurrounded(player4, player2, player3, player1, [0, 8], [8]):
+            self.vwalls[r1][c1] = False;
+            return False;
+        return True;
