b6fd919a290ee6d87e74cba2bd2c17ecd4f8afaf
MohammadReza Pakzadian <pakzadianmrp@gmail.com>
2020-07-13 19:23:17 +0430

    add multi thread to learning

--- Learning/Learn2PMultiThread.py
+++ Learning/Learn2PMultiThread.py
@@ -1,6 +1,8 @@
 import random
 import json
 from datetime import datetime
+import threading
+import queue
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -29,26 +31,32 @@ def createEarlyPopulation():
 
 
 def main():
-    for i in range(20):
+    for i in range(1):
         print(50 * "-")
         print("Round", i + 1, " of evolution")
         winners = []
         with open('population.txt', 'r') as file:
             population = json.load(file)
         random.shuffle(population)
+        winners = winners + childProduction(population)
         j = 0
-        start_t = datetime.now()
+        que = queue.Queue()
+        threads_list = list()
+        start_time = datetime.now()
         while j < 20:
-            print("finding winners of group", int(j/4+1), "...")
-            start_time = datetime.now()
-            for winner in finedWinners(population[j:j + 4]):
-                winners.append(winner)
+            print("#Thread", int(j/4+1), "finding winners of group", int(j/4+1), "...")
+            t = threading.Thread(target=lambda q, arg1, arg2: q.put(finedWinners(arg1, arg2)), args=(que, population[j:j + 4], int(j/4+1)))
+            t.start()
+            threads_list.append(t)
             j += 4
-            end_time = datetime.now()
-            print('[Duration: {}]'.format(end_time - start_time))
+        for t in threads_list:
+            t.join()
+
+        while not que.empty():
+            result = que.get()
+            winners = winners + result
         end_time = datetime.now()
         print('[Duration: {}]'.format(end_time - start_time))
-        winners = winners + childProduction(winners)
         print(winners)
         with open('population.txt', 'w') as file:
             json.dump(winners, file)
@@ -57,52 +65,48 @@ def main():
             file.write("\n")
 
 
-def finedWinners(population):
+def finedWinners(population, threadNumber):
     scores = [0, 0, 0, 0]
     numOfActions = [0, 0, 0, 0]
-    for i in range(4):
-        for j in range(i+1, 4):
-            logic = Logic2P()
-            turn = random.choice([0, 1])
-            player1 = Player(Position(0, 4))
-            player2 = Player(Position(8, 4))
-            players = [player1, player2]
-            ai1 = AI2P(logic, 8, population[i])
-            ai2 = AI2P(logic, 0, population[j])
-            while True:
-                if turn == 0:
-                    action, row, col, tim = ai1.chooseAnAction(players[0], players[1])
-                else:
-                    action, row, col, tim = ai2.chooseAnAction(players[1], players[0])
-                numOfActions[i] += 1
-                numOfActions[j] += 1
-                if action == "move":
-                    players[turn].pos.row = row
-                    players[turn].pos.col = col
-                    if turn == 1 and row == 0:
-                        winner = 1
-                        break
-                    elif turn == 0 and row == 8:
-                        winner = 0
-                        break
-                elif action == "add Vwall":
-                    if logic.addVwall(col, row, players[turn], players[0 if turn == 1 else 1], 8 if turn == 0 else 0):
-                        players[turn].walls -= 1
-                elif action == "add Hwall":
-                    if logic.addHwall(col, row, players[turn], players[0 if turn == 1 else 1],
-                                           8 if turn == 0 else 0):
-                        players[turn].walls -= 1
-                if turn == 0:
-                    turn = 1
-                else:
-                    turn = 0
+    # print(population)
+    for i in range(3):
+        que = queue.Queue()
+        threads_list = list()
+        logic = Logic2P()
+        ai1 = AI2P(logic, 8, population[0])
+        ai2 = AI2P(logic, 0, population[1+i])
+        t = threading.Thread(target=lambda q, arg1, arg2, arg3, arg4, arg5: q.put(playGame(arg1, arg2, arg3, arg4, arg5)),
+                args=(que, ai1, ai2, 1, 1+i, logic))
+        t.start()
+        threads_list.append(t)
+        if i == 0:
+            j = 2
+            k = 3
+        else:
+            j = 1
+            k = 1 + i
+        logic = Logic2P()
+        ai1 = AI2P(logic, 8, population[j])
+        ai2 = AI2P(logic, 0, population[k])
+        t = threading.Thread(
+            target=lambda q, arg1, arg2, arg3, arg4, arg5: q.put(playGame(arg1, arg2, arg3, arg4, arg5)),
+            args=(que, ai1, ai2, j, k, logic))
+        t.start()
+        threads_list.append(t)
+        for t in threads_list:
+            t.join()
+        while not que.empty():
+            result = que.get()
+            i, j, winner, actions = result
+            numOfActions[i] += actions
+            numOfActions[j] += actions
             if winner == 0:
                 scores[i] += 1
             else:
                 scores[j] += 1
 
     # print score board
-    print("\tplayer score")
+    print("\tThread", threadNumber, "players score")
     for i in range(4):
         print("\t", i+1, 4*" ", scores[i])
     m = max(scores)
@@ -132,6 +136,41 @@ def finedWinners(population):
             return [population[winners[0]], population[winners[1]]]
 
 
+def playGame(ai1, ai2, i, j, logic):
+    numOfActions = 0
+    turn = random.choice([0, 1])
+    player1 = Player(Position(0, 4))
+    player2 = Player(Position(8, 4))
+    players = [player1, player2]
+    while True:
+        if turn == 0:
+            action, row, col, tim = ai1.chooseAnAction(players[0], players[1])
+        else:
+            action, row, col, tim = ai2.chooseAnAction(players[1], players[0])
+        numOfActions += 1
+        if action == "move":
+            players[turn].pos.row = row
+            players[turn].pos.col = col
+            if turn == 1 and row == 0:
+                winner = 1
+                break
+            elif turn == 0 and row == 8:
+                winner = 0
+                break
+        elif action == "add Vwall":
+            if logic.addVwall(col, row, players[turn], players[0 if turn == 1 else 1], 8 if turn == 0 else 0):
+                players[turn].walls -= 1
+        elif action == "add Hwall":
+            if logic.addHwall(col, row, players[turn], players[0 if turn == 1 else 1],
+                              8 if turn == 0 else 0):
+                players[turn].walls -= 1
+        if turn == 0:
+            turn = 1
+        else:
+            turn = 0
+    return [i, j, winner, numOfActions]
+
+
 def childProduction(winners):
     print("generating childes ...")
     random.shuffle(winners)
@@ -140,27 +179,17 @@ def childProduction(winners):
     for i in range(0, len(winners), 2):
         break_point = random.choice([1, 2, 3])
         ch1 = []
-        ch2 = []
         for j in range(break_point):
             ch1.append(winners[i][j])
-            ch2.append(winners[i + 1][j])
         for j in range(break_point, 4):
             ch1.append(winners[i + 1][j])
-            ch2.append(winners[i][j])
         if random.random() <= 0.1:
             numOfMutations += 1
             mutation = random.choice([0, 1, 2, 3])
             ch1[mutation] = random.random()
             if ch1[mutation] == 0:
                 ch1[mutation] = 1
-        if random.random() <= 0.1:
-            numOfMutations += 1
-            mutation = random.choice([0, 1, 2, 3])
-            ch2[mutation] = random.random()
-            if ch2[mutation] == 0:
-                ch2[mutation] = 1
         childes.append(ch1)
-        childes.append(ch2)
     print("\t", numOfMutations, "mutations")
     return childes
 
